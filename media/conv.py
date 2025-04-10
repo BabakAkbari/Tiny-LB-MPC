41d0debe7ea275acd07a53b174f4d201d0b3ba6c
Babak <babakakb@gmail.com>
2025-04-10 15:23:57 -0400

    modify README.md

--- media/conv.py
+++ media/conv.py
@@ -8,7 +8,7 @@ def mp4_to_gif(mp4_file, gif_file):
     # video = video.resize(height=320)  # resize height to 320px, width is adjusted proportionally
     
     # Optional: Set a frame rate to downsample (lowering this will reduce the GIF file size)
-    video = video.set_fps(10)  # reduce the frame rate (default is 24 or higher)
+    video = video.set_fps(24)  # reduce the frame rate (default is 24 or higher)
     
     # Write the gif file
     video.write_gif(gif_file)
